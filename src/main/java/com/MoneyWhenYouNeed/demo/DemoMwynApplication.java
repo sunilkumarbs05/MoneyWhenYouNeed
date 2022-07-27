0e57ea411031f1d2eea1549fec5ec2c3f0e1d33c
sunilkumarbs <sunilkumarbs05@gmail.com>
2022-07-27 13:44:49 +0530

    initial project structure

---
+++ src/main/java/com/MoneyWhenYouNeed/demo/DemoMwynApplication.java
@@ -0,0 +1,13 @@
+package com.MoneyWhenYouNeed.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMwynApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMwynApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MoneyWhenYouNeed/demo/DemoMwynApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MoneyWhenYouNeed.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMwynApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
